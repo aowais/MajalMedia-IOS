ef8758b5c876ec1ef75faaeb47f82cee05d3bd60
Ahmad Owais <a.owais@al-ain.net>
2017-08-10 15:55:17 +0400

    fixing html font and scroll

--- MajalMedia/TestContentVC.swift
+++ MajalMedia/TestContentVC.swift
@@ -9,6 +9,22 @@
 import UIKit
 
 class TestContentVC: UIViewController {
+//    
+//    public enum HTMLNodeType : String {
+//        case HTMLUnkownNode     = ""
+//        case HTMLHrefNode       = "href"
+//        case HTMLTextNode       = "text"
+//        case HTMLCodeNode       = "code"
+//        case HTMLSpanNode       = "span"
+//        case HTMLPNode          = "p"
+//        case HTMLLiNode         = "li"
+//        case HTMLUiNode         = "ui"
+//        case HTMLImageNode      = "image"
+//        case HTMLOlNode         = "ol"
+//        case HTMLStrongNode     = "strong"
+//        case HTMLPreNode        = "pre"
+//        case HTMLBlockQuoteNode = "blockquote"
+//    }
     
     @IBOutlet weak var scrollview: UIScrollView!
     @IBOutlet weak var lbl_details: UILabel!
@@ -24,28 +40,54 @@ class TestContentVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let nid = item?.nid
         print("viewDidLoad:\(String(describing: item?.title))")
-        lbl_details.text = item?.details
-        updateViewConstraints()
+//        lbl_details.text = item?.details
+        let detailsH = lbl_details.bounds.size.height
+        let totalHeight = self.scrollview.frame.size.height+detailsH
+        print("viewDidLoad totalHeight:\(totalHeight)")
+        scrollview.contentSize = CGSize.init(width: scrollview.frame.size.width, height: totalHeight) // is a must here
+//        print("viewDidLoad detailsH:\(detailsH) nid:\(String(describing: nid))")
+//        updateViewConstraints()
         
     }
     
     override func viewWillAppear(_ animated: Bool) {
+        let nid = item?.nid
         print("viewWillAppear:\(String(describing: item?.title))")
 //        webview.loadHTMLString(dataObject as! String, baseURL: NSURL(string: "")! as URL)
 //        lbl_details.text = dataObject as? String
 //        let str = item?.details.html2AttributedString
-        lbl_details.text = item?.details
+//        lbl_details.text = item?.details
 //        lbl_details.attributedText = str
         lbl_title.text = item?.title
         lbl_room.text = item?.page_name
         lbl_date.text = item?.created_date
 //        lbl_details.isUserInteractionEnabled = false
+        var str :NSAttributedString?
+        DispatchQueue.background(background: {
+            // do something in background
+           str = self.item?.details.html2AttributedString
+        }, completion:{
+            // when background job finished, do something in main thread
+            self.lbl_details.attributedText = str
+            self.lbl_details.textAlignment = NSTextAlignment.right
+            self.lbl_details.contentMode = .scaleToFill
+            self.lbl_details.font = UIFont.systemFont(ofSize: 15)
+            self.lbl_details.adjustsFontSizeToFitWidth = true
+            self.lbl_details.sizeToFit()
+            let detailsH = self.lbl_details.bounds.size.height
+            let totalHeight = self.scrollview.frame.size.height+detailsH
+            print("totalHeight:\(totalHeight)")
+            self.scrollview.contentSize = CGSize.init(width: self.scrollview.frame.size.width, height: totalHeight)
+        })
+        
+        
         
         let detailsH = lbl_details.bounds.size.height
-        
-        print("detailsH:\(detailsH)")
-        
+        let totalHeight = self.scrollview.frame.size.height+detailsH
+        print("viewWillAppear totalHeight:\(totalHeight)")
+//        print("viewWillAppear detailsH:\(detailsH) nid:\(String(describing: nid))")
         let image = UIImage(named: "ic_placeholder")
         
         let url = URL(string: (item?.main_img)!)
@@ -55,8 +97,8 @@ class TestContentVC: UIViewController {
         img_room.kf.setImage(with: urlimg_room,placeholder: image)
         
         
-        scrollview.contentSize = CGSize.init(width: scrollview.frame.size.width, height: scrollview.frame.size.height+detailsH)
-        scrollview.isScrollEnabled = true
+//        scrollview.contentSize = CGSize.init(width: scrollview.frame.size.width, height: scrollview.frame.size.height+detailsH)
+//        scrollview.isScrollEnabled = true
     }
     
 //    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
@@ -66,23 +108,23 @@ class TestContentVC: UIViewController {
     
     override func viewDidLayoutSubviews() {
         print("viewDidLayoutSubviews\(String(describing: item?.title))")
+        let nid = item?.nid
+//        lbl_details.text = item?.details
         let detailsH = lbl_details.bounds.size.height
-        scrollview.contentSize = CGSize.init(width: scrollview.frame.size.width, height: scrollview.frame.size.height+detailsH)
+        let totalHeight = self.scrollview.frame.size.height+detailsH
+        print("viewDidLayoutSubviews totalHeight:\(totalHeight) nid:\(String(describing: nid))")
+        scrollview.contentSize = CGSize.init(width: scrollview.frame.size.width, height: totalHeight)
     }
     
     override func viewWillLayoutSubviews() {
         print("viewWillLayoutSubviews\(String(describing: item?.title))")
+        let nid = item?.nid
+//        lbl_details.text = item?.details
         let detailsH = lbl_details.bounds.size.height
+        let totalHeight = self.scrollview.frame.size.height+detailsH
+        print("viewWillLayoutSubviews totalHeight:\(totalHeight) nid:\(String(describing: nid))")
         scrollview.contentSize = CGSize.init(width: scrollview.frame.size.width, height: scrollview.frame.size.height+detailsH)
     }
-    
-//    override func viewWillAppear(_ animated: Bool) {
-//        super.viewWillAppear(animated)
-//        
-//        webview.loadHTMLString(dataObject as! String,
-//                               baseURL: NSURL(string: "") as! URL)
-//    }
-    
 
 }
 
@@ -100,3 +142,37 @@ extension String {
         return html2AttributedString?.string ?? ""
     }
 }
+
+
+extension DispatchQueue {
+    
+    static func background(delay: Double = 0.0, background: (()->Void)? = nil, completion: (() -> Void)? = nil) {
+        DispatchQueue.global(qos: .background).async {
+            background?()
+            if let completion = completion {
+                DispatchQueue.main.asyncAfter(deadline: .now() + delay, execute: {
+                    completion()
+                })
+            }
+        }
+    }
+}
+
+extension NSTextAttachment {
+    func setImageHeight(height: CGFloat) {
+        guard let image = image else { return }
+        let ratio = image.size.width / image.size.height
+        
+        bounds = CGRect(x: bounds.origin.x, y: bounds.origin.y, width: ratio * height, height: height)
+    }
+}
+
+
+
+
+//    override func viewWillAppear(_ animated: Bool) {
+//        super.viewWillAppear(animated)
+//
+//        webview.loadHTMLString(dataObject as! String,
+//                               baseURL: NSURL(string: "") as! URL)
+//    }
